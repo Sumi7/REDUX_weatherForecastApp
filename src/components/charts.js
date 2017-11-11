5bcd628466f3e59a3ac21a2411d7190f0350e9b7
Sumit <sumitsati7@gmail.com>
2017-11-12 00:22:26 +0530

    styles added with integration to google maps start

--- src/components/charts.js
+++ src/components/charts.js
@@ -9,9 +9,9 @@ function avgData(data){
 export default (props) =>{
     return (
         <div>
-            <Sparklines width = {180} height = {120}>
-                <SparklinesLine color = {this.props.color}/>
-                <SparklinesReferenceLine type = {avg}/>
+            <Sparklines width = {180} height = {120} data = {props.data}>
+                <SparklinesLine color = {props.color}/>
+                <SparklinesReferenceLine type = "avg"/>
             </Sparklines>
             <div>{avgData(props.data)} {props.unit}</div>
         </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import { weather_reducer } from './reducer_weather.js';
+import weather_reducer from './reducer_weather.js';
 
 const rootReducer = combineReducers({
   weather: weather_reducer
